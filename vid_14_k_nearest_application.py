fa66850e441485ebc3188489b38e7aeed1d607df
josiahls <jokellum@northstate.net>
2017-08-20 14:10:33 -0400

    Create a k-nearest custom classifier. Next videos apply full k nearest to the actual application

--- vid_14_k_nearest_application.py
+++ vid_14_k_nearest_application.py
@@ -29,34 +29,41 @@ from sklearn import preprocessing, model_selection, neighbors
 
 import pandas as pd
 
-# import text file as a csv
-# converts into array
-df = pd.read_csv('breast-cancer-wisconsin.data.txt')
-# convert '?' into numerical data
-df.replace('?', -999999, inplace=True)
-# remove the id column (note 1 means that we are)
-# dropping the column for each row. In theory you
-# could have 0 instead. Same behavior unless
-# there are differing column lengths
-df.drop(['id'], 1, inplace=True)
 
-# features
-X = np.array(df.drop(['class'],1))
-# labels
-y = np.array(df['class'])
+accuracies = []
 
-# sets values from model generation
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+for i in range(5):
+    # import text file as a csv
+    # converts into array
+    df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+    # convert '?' into numerical data
+    df.replace('?', -999999, inplace=True)
+    # remove the id column (note 1 means that we are)
+    # dropping the column for each row. In theory you
+    # could have 0 instead. Same behavior unless
+    # there are differing column lengths
+    df.drop(['id'], 1, inplace=True)
 
-clf = neighbors.KNeighborsClassifier()
-clf.fit(X_train, y_train)
+    # features
+    X = np.array(df.drop(['class'],1))
+    # labels
+    y = np.array(df['class'])
 
-accuracy = clf.score(X_test,y_test)
-print(accuracy)
+    # sets values from model generation
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
-# example data to test against. This will be 2
-example_measures = np.array([[4,2,1,1,1,2,3,2,1], [4,2,1,2,2,2,3,2,1]])
-example_measures = example_measures.reshape(len(example_measures),-1)
+    clf = neighbors.KNeighborsClassifier()
+    clf.fit(X_train, y_train)
 
-prediction = clf.predict(example_measures)
-print(prediction)
+    accuracy = clf.score(X_test,y_test)
+    #print(accuracy)
+
+    # example data to test against. This will be 2
+    #example_measures = np.array([[4,2,1,1,1,2,3,2,1], [4,2,1,2,2,2,3,2,1]])
+    #example_measures = example_measures.reshape(len(example_measures),-1)
+
+    #prediction = clf.predict(example_measures)
+    #print(prediction)
+    accuracies.append(accuracy)
+
+print(sum(accuracies)/len(accuracies))
--- vid_16_17_18_19_custom_k_nearest.py
+++ vid_16_17_18_19_custom_k_nearest.py
@@ -38,25 +38,33 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 from collections import Counter
 import warnings
+import pandas as pd
+import random
 
-style.use('fivethirtyeight')
 
-dataset = {'k': [[1, 2], [2, 3], [3, 1]], 'r': [[6, 5], [7, 7], [8, 6]]}
-new_features = [5, 7]
+# style.use('fivethirtyeight')
+
+# dataset = {'k': [[1, 2], [2, 3], [3, 1]], 'r': [[6, 5], [7, 7], [8, 6]]}
+# new_features = [5, 7]
 
 # or [[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
-#for i in dataset:
- #   for ii in dataset[i]:
-  #      plt.scatter(ii[0], ii[1], s=100, color=i)
+# for i in dataset:
+#   for ii in dataset[i]:
+#      plt.scatter(ii[0], ii[1], s=100, color=i)
 
-#plt.scatter(new_features[0],new_features[1])
-#plt.show()
+# plt.scatter(new_features[0],new_features[1])
+# plt.show()
 
 # euclid_distance = sqrt((plot1[0] - plot2[0])**2 + (plot1[1] -plot2[1])**2)
 
-#print(euclid_distance)
+# print(euclid_distance)
 
+# data is thedata weare running k nearest on
+# predict is the features we want to group
+# k is the number of comparison we want to make with the dataset
 def k_nearest_neighbors(data, predict, k=3):
+    # checks if the dataset is stupidly low
+    # like 2 sets of data when k is 3
     if len(data) >= k:
         warnings.warn('K is set to a value less than total voting groups idiot')
 
@@ -64,7 +72,7 @@ def k_nearest_neighbors(data, predict, k=3):
     for group in data:
         for features in data[group]:
             # alt: euclidean_distance = np.linalg.norm(np.array(features)-np.array(predict))
-            euclidean_distance = np.sqrt(np.sum((np.array(features) - np.array(predict))**2))
+            euclidean_distance = np.sqrt(np.sum((np.array(features) - np.array(predict)) ** 2))
             # creates a list of distances and groups
             distances.append([euclidean_distance, group])
 
@@ -72,12 +80,16 @@ def k_nearest_neighbors(data, predict, k=3):
     # for i in sorted(distances)[:k]:
     #   i[1]
 
-    print(Counter(votes).most_common(1))
+    # print(Counter(votes).most_common(1))
     vote_result = Counter(votes).most_common(1)[0][0]
+    confidence = Counter(votes).most_common(1)[0][1] / k
 
-    return vote_result
+    #print(vote_result, confidence)
+
+    return vote_result, confidence
 
 
+"""
 result= k_nearest_neighbors(dataset, new_features, k=3)
 print(result)
 
@@ -87,3 +99,47 @@ for i in dataset:
 
 plt.scatter(new_features[0],new_features[1], color=result)
 plt.show()
+"""
+accuracies = []
+
+for i in range(5):
+    df = pd.read_csv("breast-cancer-wisconsin.data.txt")
+    df.replace('?', -99999, inplace=True)
+    df.drop(['id'], 1, inplace=True)
+    # convert to a float (makes sure all values are numeric)
+    full_data = df.astype(float).values.tolist()
+
+    random.shuffle(full_data)
+
+    test_size = 0.2
+    train_set = {2: [], 4: []}
+    test_set = {2: [], 4: []}
+    # first 80% of data
+    train_data = full_data[:-int(test_size * len(full_data))]
+    # last 20% of data
+    test_data = full_data[-int(test_size * len(full_data)):]
+
+    for i in train_data:
+        # appending lists into this list
+        train_set[i[-1]].append(i[:-1])
+
+    for i in test_data:
+        # appending lists into this list
+        test_set[i[-1]].append(i[:-1])
+
+    correct = 0
+    total = 0
+
+    for group in test_set:
+        for data in test_set[group]:
+            vote, confidence = k_nearest_neighbors(train_set, data, k=5)
+            if group == vote:
+                correct += 1
+            #else:
+              #  print(confidence)
+            total += 1
+
+    print('Accuracy:', correct / total)
+    accuracies.append(correct/total)
+
+print(sum(accuracies) / len(accuracies))
