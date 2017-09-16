5cdb795f9101418ee08c15958596d886637d9e09
josiahls <jokellum@northstate.net>
2017-09-16 14:43:13 -0400

    Starting tensor flow

---
+++ vid_43_CNN_intro.py
@@ -0,0 +1,20 @@
+"""
+CNN THEORY:
+
+Everyhting startsfromthe concept of a neuron.
+It is conposed of dendrits which go to the neuron,
+and the axon which outputs to another denarites.
+the connection between these is the synapse
+
+Neural network doesnt actually use any of these term tehe
+So if the neoron gets a set of inputs, each one has its definition of
+what makes it fire.
+
+SO you might have something like:
+x1 -> weight1 ->
+x2 -> weight2 -> sum(x1,x2,x3) -> f(step function that has a limit to make it fire (1))
+x3 -> weight3 ->
+
+
+
+"""
