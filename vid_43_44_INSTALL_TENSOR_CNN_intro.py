0678eb42ab2d4724862de74597ae8be1a6f9f0a1
josiahls <jokellum@northstate.net>
2017-09-16 17:01:08 -0400

    Finished tesnorflow NN for the mnist dataset. As accuracy of 95%. Modified to fit TensorFlow 1.0 >

--- vid_43_44_INSTALL_TENSOR_CNN_intro.py
+++ vid_43_44_INSTALL_TENSOR_CNN_intro.py
@@ -49,10 +49,51 @@ The main point of failure is the data sets.
 
 Note Machine learning reddit can be helpful.
 
+INSTALL TENSOR FLOW
 
+*Note that following:
+https://www.tensorflow.org/install/install_sources
+Shows you how to install and build tensor flow to compile with your
+specifc architecture. Just following the instructions below
+installs rebuilt tensflow packages that may not be optimized for
+your system.
 
+1. sudo apt-get install python3-pip python3-dev
+2. a. pip3 install tensorflow     # Python 3.n; CPU support (no GPU support)
+2. b. pip3 install tensorflow-gpu # Python 3.n; GPU support
+3. (optional) sudo pip3 install --upgrade tfBinaryURL   # Python 3.n
+4. (optional) sudo pip3 uninstall tensorflow # for Python 3.n
+5. (optional) sudo apt-get install htop
 
-
-
-
+GOTO:
+https://www.tensorflow.org/install/install_linux
+for installation help
 """
+
+import tensorflow as tf
+
+x1 = tf.constant(5)
+x2 = tf.constant(6)
+
+# note mul is depreciated
+result = tf.multiply(x1, x2)
+
+# note this just creates the tensor
+print(result)
+
+# this is to actually run the compile tensor code
+sess = tf.Session()
+print(sess.run(result))
+# closes the object o free ram
+sess.close()
+
+# or you can do:
+# which will automatically close
+with tf.Session() as sess:
+    output = sess.run(result)
+    print(output)
+
+# The following line will not work
+# print(output)
+
+
---
+++ vid_45_deep_learn_net.py
@@ -0,0 +1,115 @@
+"""
+Note each pixel will be a feature
+Remmber earlier when we had features x1,x2,x3
+
+input > weight > hidden layer 1 (activation function) > weights > hidden layer 2
+(activation function) > weights > output layer
+
+compare the output with the intended output > cost or loss function (cross entropy) (how far off we are)
+optimization function (optimizer) > minimize cost (AdamOptimizer...SGD... AdaGrad) (goes back through the network to
+manipulate the weights) ie backpropagation
+
+backpropagation
+
+feedforward + backprop = epoch
+"""
+
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+# one_hot means that one is on and the rest are off
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+# 10 classes, 0-9
+'''
+0=0
+1=1
+2=2
+Well one_hot goes and makes
+0 = [1,0,0,0,0,0,0,0]
+1 = [0,1,0,0,0,0,0,0]
+2 = [0,0,1,0,0,0,0,0]
+3 = [0,0,0,1,0,0,0,0]
+...
+'''
+
+# number of hidden layers
+n_nodeshl1 = 500
+n_nodeshl2 = 500
+n_nodeshl3 = 500
+
+n_classes = 10
+# so this creates batches of 100 images
+batch_size = 100
+
+# height x width
+# we flatten the picture out into
+# 784 pixels long
+x = tf.placeholder('float', [None, 784])
+y = tf.placeholder('float')
+
+
+def neural_network_model(data):
+    # creates an arrayof weights in one giant tensor
+    # biases are somthing added in after the weights
+    # so
+    # input_data * weights + biases
+    hidden_1_layer = {'weights': tf.Variable(tf.random_normal([784, n_nodeshl1])),
+                      'biases': tf.Variable(tf.random_normal([n_nodeshl1]))}
+
+    hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodeshl1, n_nodeshl2])),
+                      'biases': tf.Variable(tf.random_normal([n_nodeshl2]))}
+
+    hidden_3_layer = {'weights': tf.Variable(tf.random_normal([n_nodeshl2, n_nodeshl3])),
+                      'biases': tf.Variable(tf.random_normal([n_nodeshl3]))}
+
+    output_layer = {'weights': tf.Variable(tf.random_normal([n_nodeshl3, n_classes])),
+                    'biases': tf.Variable(tf.random_normal([n_classes]))}
+
+    l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
+    # relu is the activation function
+    l1 = tf.nn.relu(l1)
+
+    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
+    # relu is the activation function
+    l2 = tf.nn.relu(l2)
+
+    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
+    # relu is the activation function
+    l3 = tf.nn.relu(l3)
+
+    output = tf.add(tf.matmul(l3, output_layer['weights']), output_layer['biases'])
+
+    return output
+
+
+def train_neural_network(X):
+    prediction = neural_network_model(X)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+
+    # we want to minimize the cost
+    # has a param is the learning rate that is 0.001
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+    # cycles of feed forward and + backprop
+    num_epochs = 10
+    with tf.Session() as sess:
+        # the init all var initializes as the variables
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(num_epochs):
+            epoch_loss = 0
+            for _ in range(int(mnist.train.num_examples / batch_size)):
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+                epoch_loss += c
+            print('Epoch', epoch, ' completed out of ', num_epochs, ' Loss: ', epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.arg_max(y, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy: ', accuracy.eval({X: mnist.test.images, y: mnist.test.labels}))
+
+
+train_neural_network(x)
