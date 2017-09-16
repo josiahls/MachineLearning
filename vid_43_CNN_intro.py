de55615a42433d4f958175a0d15dead4dc4ce7e2
josiahls <jokellum@northstate.net>
2017-09-16 14:44:44 -0400

    Starting tensor flow

--- vid_43_CNN_intro.py
+++ vid_43_CNN_intro.py
@@ -12,8 +12,12 @@ what makes it fire.
 
 SO you might have something like:
 x1 -> weight1 ->
-x2 -> weight2 -> sum(x1,x2,x3) -> f(step function that has a limit to make it fire (1))
+x2 -> weight2 -> sum(x1,x2,x3) -> f(step function that has a limit to make it fire (1)) -> 0 or 1 and goesto the next neuron
 x3 -> weight3 ->
+
+Usually people dont use a step function. They might use a sigmouyed function which is more s shaped
+which is an activation function
+
 
 
 
